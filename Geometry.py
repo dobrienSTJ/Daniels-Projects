5c0c804f2d9f83fd71b61feee191f7fbaa621a65
Daniel O'Brien <15dobrien@stjosephsrush.com>
2017-05-17 18:18:20 +0100

    MATHS

---
+++ Geometry.py
@@ -0,0 +1,69 @@
+#PLEASE REMEMBER TO INSERT: name_of_code_here(numbers_you_are_calculating)
+#THANKS!!!!!!!!!!!!!!!!!
+
+
+
+
+#Triangle area the last maths function program
+def trianglearea(base,height):
+    print("Ok so you want the area of a triangle.....")
+    print("Did you Remember the 0.5!!!")
+    answer = 0.5*base*height
+    print(answer) 
+        
+
+#The base is always halved or multipled by 0.5
+
+
+
+
+
+#This time a cylinder function with proper variables
+def cylindervolume(radius,height):
+    print("Ok so you want the volume of a cylinder.....")
+    print("Did you Remember the pi!!!")
+    answer = 3.14*radius**2*height
+    print(answer) 
+        
+
+#and an area to add in radius and height with , in between
+
+
+
+
+
+
+
+
+
+
+
+
+#function just below here with proper maths equation
+def circlearea(radius):
+    print("Ok so you want the area of a circle.....")
+    print("Did you Remember the r squared!!!")
+    answer = 3.14*radius**2
+    print(answer) 
+        
+
+#and once again remember to addd in your numbers.......
+
+
+
+
+
+
+
+
+
+
+#The function is below, note the maths 4 lines down and the new ()
+def circlearea(radius):
+    print("Ok so you want the circumfrence of a circle.....")
+    print("Did you Remember pi!!!")
+    answer = 2*3.14*radius
+    print(answer) 
+        
+
+#You enter in your numbers here!!!!!!!!
