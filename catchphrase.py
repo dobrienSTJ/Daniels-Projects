faa376b7902995d1f6793edd70b02af062d7d72a
Daniel O'Brien <15dobrien@stjosephsrush.com>
2017-05-17 18:16:11 +0100

    Your Name: ____________

---
+++ catchphrase.py
@@ -0,0 +1,12 @@
+print("Make your own catchphrase!")
+
+name=input("What is your name?")
+hobby=input("What is your faviourite hobby?")
+colour=input("What is your faviourite colour?")
+number-input("What is your lucky number?")
+job=input("Where do you work?")
+home=input("Where do you live?")
+place=input("Where are you from?")
+pet=input("What's your pets name?")
+
+print "Your name is %s, you love %s, your colour is %s, your lucky number is %s, you work in a %s, you live in %s, you are from %s and your pet is called %s " (name, hobby, colour, number, job, home, place, pet)
