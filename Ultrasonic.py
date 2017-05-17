8ad85835f9cec7028f5204781e7c434f0e85dfc6
Daniel O'Brien <15dobrien@stjosephsrush.com>
2017-05-17 18:03:53 +0100

    Ultrasonic

---
+++ Ultrasonic.py
@@ -0,0 +1,32 @@
+import Rpi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+TRIG = 12
+ECHO = 24
+
+GPIO.setup(TRIG,GPIO.OUT)
+GPIO.output(TRIG,0)
+
+GPIO.setup(ECHO,GPIO.IN)
+
+time.sleep(0.1)
+
+print("Starting Measurement....")
+
+GPIO.output(TRIG,1)
+time.sleep(0.00001)
+GPIO.output(TRIG,0)
+
+while GPIO.input(ECHO) == 0:
+				pass
+start = time.time()
+
+while GPIO.input(ECHO) == 1:
+				pass
+stop = time.time()
+
+print (stop - start) * 17000
+
+GPIO.cleanup()
