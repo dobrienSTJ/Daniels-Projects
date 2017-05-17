d52dde739ceb2febc860e01a04172c055b3667dc
Daniel O'Brien <15dobrien@stjosephsrush.com>
2017-05-17 18:15:08 +0100

    Password

---
+++ qwerty.py
@@ -0,0 +1,12 @@
+#High End Python security!!!!!!
+password=input("Please enter the password to continue..")
+
+if password != "qwerty":
+    print("Access Denied")
+
+elif password == "qwerty":
+    print("Welcome")
+                
+
+end=input("END OF PROGRAM")
+        
