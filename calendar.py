47aa7c1a172fc6bf101769c81822bb0a672e01c7
Daniel O'Brien <15dobrien@stjosephsrush.com>
2017-05-17 18:15:38 +0100

    Plan Ahead!

---
+++ calendar.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+print("Coding calendar for October!") 
+now = datetime.now()
+
+day = str(now.day)
+
+
+
+    if day == "3" or "6" or "10" or "17" or "13" or "20":
+        print("You Have Coding Today!!!!!!")
+
+    else:
+        print("ERROR")
