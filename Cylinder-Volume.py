ea702c68b5fa370253b17deb33b8db9795a29d0b
Daniel O'Brien <15dobrien@stjosephsrush.com>
2017-05-18 15:06:09 +0100

    Create Cylinder-Volume.py

---
+++ Cylinder-Volume.py
@@ -0,0 +1,9 @@
+def cylindervolume(radius,height):
+    print("Ok so you want the volume of a cylinder.....")
+    print("Did you Remember the pi!!!")
+    answer = 3.14*radius**2*height
+    print(answer) 
+        
+
+
+cylindervolume()   
