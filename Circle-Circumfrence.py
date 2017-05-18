7f0109b9fbe782bca06d3cbcb2cd5259796394ad
Daniel O'Brien <15dobrien@stjosephsrush.com>
2017-05-18 15:09:59 +0100

    Create Circle-Circumfrence.py

---
+++ Circle-Circumfrence.py
@@ -0,0 +1,9 @@
+def circlearea(radius):
+    print("Ok so you want the circumfrence of a circle.....")
+    print("Did you Remember pi!!!")
+    answer = 2*3.14*radius
+    print(answer) 
+        
+
+
+circlearea()
